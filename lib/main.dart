35ec00f48daea44bb80b9f30dad6f6c394a8bbf6
EevnxX <miftamartin1@gmail.com>
2024-05-15 01:57:53 -0700

    Melakukan perkembangan pertama pada aplikasi untuk ujian saya

--- lib/main.dart
+++ lib/main.dart
@@ -58,53 +58,205 @@ class MyApp extends StatelessWidget {
             child: Container(
               child: Column(
                 children: <Widget>[
+                  FractionallySizedBox(
+                    widthFactor: 0.9,
+                    child: Container(
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Container(
+                                  child: Text(
+                                    'Rabu',
+                                    style: TextStyle(
+                                      fontSize: 19,
+                                      fontWeight: FontWeight.w700,
+                                      color: Colors.teal[800],
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  child: Text(
+                                    '15\nMEI',
+                                    style: TextStyle(
+                                        fontSize: 35,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.teal[800],
+                                        height: 1.2),
+                                  ),
+                                  margin: EdgeInsets.only(left: 11),
+                                ),
+                              ],
+                            ),
+                            margin: EdgeInsets.only(right: 40),
+                          ),
+                          Container(
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  child: Text(
+                                    'Catatan Hari Ini:',
+                                    style: TextStyle(
+                                      fontSize: 19,
+                                      fontWeight: FontWeight.w700,
+                                      color: Colors.teal[800],
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  child: Text(
+                                    'Mata Kuliah Pengujian Pemeliharaan Sistem Dan Mobile Programming',
+                                    style: TextStyle(
+                                      color: Colors.teal.shade800,
+                                    ),
+                                  ),
+                                  width: 170,
+                                  margin: EdgeInsets.only(left: 30, top: 10),
+                                ),
+                              ],
+                            ),
+                            decoration: BoxDecoration(
+                              border: Border(
+                                left: BorderSide(
+                                  width: 3,
+                                  color: Colors.teal.shade800,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      height: 140,
+                      margin: EdgeInsets.only(top: 20),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.teal[200],
+                      ),
+                    ),
+                  ),
                   Container(
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
                         Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Container(
+                                child: Icon(
+                                  Icons.gamepad,
+                                  size: 60,
+                                ),
+                              ),
+                              Container(
+                                child: Text(
+                                  'Permainan',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 12,
+                          ),
                           height: 110,
                           width: 110,
                           decoration: BoxDecoration(
-                            color: Colors.black,
+                            color: Colors.deepOrangeAccent,
                             shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(10),
                           ),
                         ),
                         Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Container(
+                                child: Icon(
+                                  Icons.school,
+                                  size: 60,
+                                ),
+                              ),
+                              Container(
+                                child: Text(
+                                  'MYCIC',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 12,
+                          ),
                           height: 110,
                           width: 110,
                           decoration: BoxDecoration(
-                            color: Colors.black,
+                            color: Colors.deepOrangeAccent,
                             shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(10),
                           ),
                         ),
                         Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Container(
+                                child: Icon(
+                                  Icons.menu_book_sharp,
+                                  size: 60,
+                                ),
+                              ),
+                              Container(
+                                child: Text(
+                                  'Pembelajaran',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 12,
+                          ),
                           height: 110,
                           width: 110,
                           decoration: BoxDecoration(
-                            color: Colors.black,
+                            color: Colors.deepOrangeAccent,
                             shape: BoxShape.rectangle,
+                            borderRadius: BorderRadius.circular(10),
                           ),
                         ),
                       ],
                     ),
-                    margin: EdgeInsets.only(bottom: 20),
-                    height: 200,
+                    height: 160,
                     width: MediaQuery.of(context).size.width,
                   ),
                   Container(
                     child: FractionallySizedBox(
                       widthFactor: 0.9,
                       child: Container(
-                        margin: EdgeInsets.only(bottom: 20),
-                        height: 130,
+                        height: 200,
                         decoration: BoxDecoration(
                           color: Colors.blueAccent,
                           shape: BoxShape.rectangle,
                         ),
                       ),
                     ),
-                    margin: EdgeInsets.only(bottom: 40),
+                    margin: EdgeInsets.only(bottom: 30),
                   ),
                   FractionallySizedBox(
                     widthFactor: 0.9,
